c67bf8d259e1feade0a627353801a1bfbafacfd1
thuan <thuan.hoang@vastbit.com>
2021-12-20 17:15:23 +0700

    aaaa

--- lib/esm/HelloWorld.d.ts
+++ lib/esm/HelloWorld.d.ts
@@ -1,7 +1,4 @@
 import * as React from "react";
-
 export default class HelloWorld extends React.Component<any, any> {
-  renders() {
-    return <div style={{ color: this.props.color }}>Hello world!</div>;
-  }
+    renders(): JSX.Element;
 }
